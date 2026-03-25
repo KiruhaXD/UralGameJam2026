1ca1a26d780862777ed2925dfc5604e40e278899
Kirill Vinokurov <131180897+KiruhaXD@users.noreply.github.com>
2026-03-25 06:45:29 +0300

    edit scripts

--- Assets/Scripts/EnemyScripts/ShowEnemysWaves.cs
+++ Assets/Scripts/EnemyScripts/ShowEnemysWaves.cs
@@ -51,6 +51,7 @@ public class ShowEnemysWaves : MonoBehaviour
                 spawnEffectsManager.SpawnEffectFire();
                 ShowFourthWave();
             }
+            
         }
 
         if (enemyBossFourthWave.isDeadEnemy == true)
--- Assets/Scripts/EnemyScripts/SpawnEffectsManager.cs
+++ Assets/Scripts/EnemyScripts/SpawnEffectsManager.cs
@@ -12,40 +12,62 @@ public class SpawnEffectsManager : MonoBehaviour
 
     [SerializeField] ChoiseEffectController choiseEffectController;
 
+    [SerializeField] Transform spawnPointEffectPosition;
+
+    [SerializeField] Transform playerPosition;
+
     //[SerializeField] ParticleSystem[] spawnEffects;
 
     public void SpawnEffectIce() 
     {
+        enemysEffects[0].transform.position = spawnPointEffectPosition.position;
+
         enemysEffects[0].gameObject.SetActive(true);
+
+        spawnPointEffectPosition.parent = enemysEffects[0].transform;
 
         if (choiseEffectController.isPickUpEffectIce == true) 
         {
             //spawnEffects[0].Stop();
             enemysEffects[0].gameObject.SetActive(false);
+
+            spawnPointEffectPosition.parent = playerPosition;
         }
 
     }
 
     public void SpawnEffectShock() 
     {
+        enemysEffects[1].transform.position = spawnPointEffectPosition.position;
+
         enemysEffects[1].gameObject.SetActive(true);
+
+        spawnPointEffectPosition.parent = enemysEffects[1].transform;
 
         if (choiseEffectController.isPickUpEffectShock == true) 
         {
             //spawnEffects[1].Stop();
             enemysEffects[1].gameObject.SetActive(false);
+
+            spawnPointEffectPosition.parent = playerPosition;
         }
 
     }
 
     public void SpawnEffectFire() 
     {
+        enemysEffects[2].transform.position = spawnPointEffectPosition.position;
+
         enemysEffects[2].gameObject.SetActive(true);
+
+        spawnPointEffectPosition.parent = enemysEffects[2].transform;
 
         if (choiseEffectController.isPickUpEffectFire == true) 
         {
             //spawnEffects[2].Stop();
             enemysEffects[2].gameObject.SetActive(false);
+
+            spawnPointEffectPosition.parent = playerPosition;
         }
 
     } 
